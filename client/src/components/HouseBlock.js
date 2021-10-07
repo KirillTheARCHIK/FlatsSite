0b8146c3b93d943a887a5d5eb9dcc173e7d62cae
KirillTheARCHIK <kirillthearchik@yandex.ru>
2021-10-07 22:42:49 +0400

    1

--- client/src/components/HouseBlock.js
+++ client/src/components/HouseBlock.js
@@ -1,12 +1,14 @@
 import { observer } from 'mobx-react-lite'
 import {Context} from '../index'
 import serverUrl from '../env.js'
+import {useHistory} from "react-router-dom"
 
 const HouseBlock=({house})=>{
+    const history=useHistory()
     return(
         <div className="HouseBlock">
             <img src={serverUrl+house.image} height="200" width="310"/>
-            <h1>{house.name}</h1>
+            <h1 onClick={() => history.push('Houses/' + house.id)}>{house.name}</h1>
             <p>Квартиры от {house.startPrice} млн. рублей</p>
         </div>
     )
